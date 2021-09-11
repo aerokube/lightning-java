5367421dd7b53cdbb389a38eab8dd4d2c46311ef
Ivan Krutov <vania-pooh@aerokube.com>
2021-09-11 14:38:53 +0300

    Removed useless setCapability methods

--- src/main/java/com/aerokube/lightning/Capabilities.java
+++ src/main/java/com/aerokube/lightning/Capabilities.java
@@ -14,18 +14,6 @@ public class Capabilities {
         capabilities.put(JSON_PROPERTY_BROWSER_NAME, browserName);
     }
 
-    public void setCapability(String key, String value) {
-        capabilities.put(key, value);
-    }
-
-    public void setCapability(String key, int value) {
-        capabilities.put(key, value);
-    }
-
-    public void setCapability(String key, boolean value) {
-        capabilities.put(key, value);
-    }
-
     public void setCapability(String key, Object value) {
         capabilities.put(key, value);
     }
